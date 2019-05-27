51e2bdfc276208ca5784afaaa4c98d6ca39c1d9e
xichiou <xichiou@gmail.com>
2019-05-27 20:55:03 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -17,9 +17,9 @@ rm lamp-xoops.zip
 wget --no-check-certificate https://github.com/xichiou/lamp-xoops/archive/master.zip -O lamp-xoops.zip
 if [ -f lamp-xoops.zip ]; then
   unzip -q -o lamp-xoops.zip
-  cd lamp-xoops-master/
+  cd lamp-xoops-master
   chmod +x *.sh
-  rm lampxoops.zip
+  rm lamp-xoops.zip
   echo -e "\n已經取得最新 lamp_xoops 程式腳本!!\n"
 else
   echo -e "更新失敗"
