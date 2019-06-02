ae6b1b1b0f536853755b20a21dd2553a26e8684c
xichiou <xichiou@gmail.com>
2019-06-02 22:44:01 +0800

    調整介面

--- upgrade_xoops.sh
+++ upgrade_xoops.sh
@@ -26,6 +26,34 @@ get_char()
     stty $SAVEDSTTY
 }
 
+get_yes_no()
+{
+  while true
+  do
+    echo -n "$1 [y/n]"
+    ANSER=$(get_char)
+    case $ANSER in
+        y|Y)
+        echo ""
+        if [ $# -ge 2 ]; then
+          echo "-----------------------------"
+          echo "$2"
+          echo "-----------------------------"
+        fi
+        return 1
+        break
+        ;;
+        n|N)
+        echo ""
+        return 0
+        break
+        ;;
+        *)
+        echo -e "\t請輸入 y 或 n"
+    esac
+  done
+}
+
 
 SEARCH_PATH="/var/www/html"
 
@@ -45,7 +73,7 @@ else
   fi
 fi
 
-#echo -e "\n檢查網站目錄 $SEARCH_PATH ..."
+echo -e "\n檢查網站目錄 $SEARCH_PATH ..."
 
 if [ ! -f "$SEARCH_PATH/mainfile.php" ]; then
   echo "錯誤! $SEARCH_PATH 目錄下沒有 mainfile.php，請確認網站放在哪個目錄後重新執行"
@@ -59,7 +87,7 @@ cd $SEARCH_PATH
 XOOPS_ROOT_PATH=$(cat mainfile.php |grep "define('XOOPS_ROOT_PATH'"|cut -d"," -f 2|cut -d"'" -f 2)
 echo -e "[設定]XOOPS_ROOT_PATH: \e[32m$XOOPS_ROOT_PATH\e[0m"
 if [ $XOOPS_ROOT_PATH != $SEARCH_PATH ]; then
-  echo "XOOPS_ROOT_PATH 和實際的網站資料夾不同，可能無法運作!"
+  echo "注意：XOOPS_ROOT_PATH 和實際存放的位置 $SEARCH_PATH 不同!"
   exit 1002
 fi
 
@@ -72,40 +100,74 @@ echo -e "[設定]XOOPS_PATH: \e[32m$XOOPS_PATH\e[0m"
 XOOPS_URL=$(cat mainfile.php |grep "define('XOOPS_URL"|sed 's/^[ \t]*//g'|sed -e '/^\/\//d'|grep "define('XOOPS_URL'"|cut -d"," -f 2|cut -d"'" -f 2)
 echo -e "[設定]XOOPS_URL: \e[32m$XOOPS_URL\e[0m"
 
+NEED_UPGRADE=0
 TAD_ADM_VERSION_CURRENT=$(cat modules/tad_adm/xoops_version.php |grep "'version"|cut -d"=" -f 2|cut -d";" -f 1|cut -d"'" -f 2)
-echo -e "[模組]站長工具箱 版本: \e[32m${TAD_ADM_VERSION_CURRENT}\e[0m"
-TAD_ADM_VERSION_CURRENT=$(echo $TAD_ADM_VERSION_CURRENT|sed 's/\.//g')
-TAD_ADM_VERSION_CURRENT=$(($TAD_ADM_VERSION_CURRENT))
-if [ $TAD_ADM_VERSION_CURRENT -lt 100 ]
+echo -e "[模組]站長工具箱 版本: \e[32m${TAD_ADM_VERSION_CURRENT}\e[0m\c"
+TAD_ADM_VERSION_CURRENT_NUM=$(echo $TAD_ADM_VERSION_CURRENT|sed 's/\.//g')
+TAD_ADM_VERSION_CURRENT_NUM=$(($TAD_ADM_VERSION_CURRENT_NUM))
+if [ $TAD_ADM_VERSION_CURRENT_NUM -lt 100 ]
 then
-    TAD_ADM_VERSION_CURRENT=$(($TAD_ADM_VERSION_CURRENT*10))
+    TAD_ADM_VERSION_CURRENT_NUM=$(($TAD_ADM_VERSION_CURRENT_NUM*10))
 fi
-#echo $TAD_ADM_VERSION_CURRENT
+#echo $TAD_ADM_VERSION_CURRENT_NUM
+if [ $TAD_ADM_VERSION_CURRENT_NUM -lt 281 ]
+then
+    echo -e "\e[31m...需要更新\e[0m\c"
+    NEED_UPGRADE=1
+fi
+echo ""
 
 TADTOOLS_VERSION_CURRNET=$(cat modules/tadtools/xoops_version.php |grep "'version"|cut -d"=" -f 2|cut -d";" -f 1|cut -d"'" -f 2)
-echo -e "[模組]tadtools 版本: \e[32m${TADTOOLS_VERSION_CURRNET}\e[0m"
-TADTOOLS_VERSION_CURRNET=$(echo $TADTOOLS_VERSION_CURRNET|sed 's/\.//g')
-TADTOOLS_VERSION_CURRNET=$(($TADTOOLS_VERSION_CURRNET))
-if [ $TADTOOLS_VERSION_CURRNET -lt 100 ]
+echo -e "[模組]tadtools 版本: \e[32m${TADTOOLS_VERSION_CURRNET}\e[0m\c"
+TADTOOLS_VERSION_CURRNET_NUM=$(echo $TADTOOLS_VERSION_CURRNET|sed 's/\.//g')
+TADTOOLS_VERSION_CURRNET_NUM=$(($TADTOOLS_VERSION_CURRNET_NUM))
+if [ $TADTOOLS_VERSION_CURRNET_NUM -lt 100 ]
 then
-    TADTOOLS_VERSION_CURRNET=$(($TADTOOLS_VERSION_CURRNET*10))
+    TADTOOLS_VERSION_CURRNET_NUM=$(($TADTOOLS_VERSION_CURRNET_NUM*10))
 fi
-#echo $TADTOOLS_VERSION_CURRNET
+#echo $TADTOOLS_VERSION_CURRNET_NUM
+if [ $TADTOOLS_VERSION_CURRNET_NUM -lt 326 ]
+then
+    echo -e "\e[31m...需要更新\e[0m\c"
+    NEED_UPGRADE=1
+fi
+echo ""
 
 XOOPS_VERSION_CURRENT=$(cat include/version.php |grep "'XOOPS_VERSION"|cut -d"," -f 2|cut -d"'" -f 2)
-echo -e "[核心]XOOPS: \e[32m${XOOPS_VERSION_CURRENT}\e[0m"
-XOOPS_VERSION_CURRENT=$(echo $XOOPS_VERSION_CURRENT|cut -d"." -f 3) #只留下第三個數字
-XOOPS_VERSION_CURRENT=$(($XOOPS_VERSION_CURRENT))
-#echo $XOOPS_VERSION_CURRENT
+echo -e "[核心]XOOPS: \e[32m${XOOPS_VERSION_CURRENT}\e[0m\c"
+XOOPS_VERSION_CURRENT_NUM=$(echo $XOOPS_VERSION_CURRENT|cut -d"." -f 3) #只留下第三個數字
+XOOPS_VERSION_CURRENT_NUM=$(($XOOPS_VERSION_CURRENT_NUM))
+#echo $XOOPS_VERSION_CURRENT_NUM
+if [ $XOOPS_VERSION_CURRENT_NUM -lt 9 ]
+then
+    echo -e "\e[31m...需要更新\e[0m\c"
+    NEED_UPGRADE=1
+fi
+echo ""
 
-echo -e "\n按下任一按鍵開始安裝更新...或是按下 Ctrl+C 取消安裝"
-char=`get_char`
+if [ -f "${XOOPS_ROOT_PATH}/class/xoopsform/renderer/XoopsFormRendererBootstrap4.php" ]; then
+  echo -e "[補丁]BootStrap4升級補丁 \e[32m已經安裝\e[0m"
+else
+  echo -e "[補丁]BootStrap4升級補丁...\e[31m...需要安裝\e[0m"
+  NEED_UPGRADE=1
+fi
+
+if [ $NEED_UPGRADE == 0 ]; then
+  echo -e "\n恭喜! 沒有需要更新的項目\n"
+  exit 0;
+fi
+
+
+# get_yes_no "關閉這台伺服器 IPV6 網路功能，你要關閉?"
+get_yes_no "你要開始安裝更新?" "執行安裝更新"
+if [ $? -eq 0 ]; then exit 1; fi
+
 
 ####################### 進行更新 ########################
 cd "$XOOPS_ROOT_PATH/modules"
 
 MESSAGE=""
-if [ $TAD_ADM_VERSION_CURRENT -lt 281 ]; then
+if [ $TAD_ADM_VERSION_CURRENT_NUM -lt 281 ]; then
   echo "進行更新[模組]站長工具箱 ==> 2.81"
   echo "下載模組並解開： tad_adm 站長工具箱..."
   if ! [ -f tad_adm_${TAD_ADM_VERSION}.zip ];then
@@ -120,7 +182,7 @@ if [ $TAD_ADM_VERSION_CURRENT -lt 281 ]; then
   fi
 fi
 
-if [ $TADTOOLS_VERSION_CURRNET -lt 326 ]; then
+if [ $TADTOOLS_VERSION_CURRNET_NUM -lt 326 ]; then
   echo "進行更新[模組]tadtools 工具包 ==> 3.26"
   echo "下載模組並解開： tadtools 工具包..."
   if ! [ -f tadtools_${TADTOOLS_VERSION}.zip ];then
@@ -152,7 +214,7 @@ if [ ! -f "${XOOPS_ROOT_PATH}/class/xoopsform/renderer/XoopsFormRendererBootstra
 fi
 
 
-if [ $XOOPS_VERSION_CURRENT -lt 9 ]; then
+if [ $XOOPS_VERSION_CURRENT_NUM -lt 9 ]; then
   echo "進行更新[核心]XOOPS  ==> XOOPS_CORE"
   wget "http://120.115.2.90/modules/tad_uploader/index.php?op=dlfile&cfsn=146&cat_sn=16&name=xoopscore25-2.5.9_tw_for_upgrade_20170803.zip" -O xoopscore25-2.5.9_tw_for_upgrade_20170803.zip
   if [ -f xoopscore25-2.5.9_tw_for_upgrade_20170803.zip ];then
