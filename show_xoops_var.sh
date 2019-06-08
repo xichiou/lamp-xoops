b25ce3f8441ae126580a0b5bc0e53589d5a00141
xichiou <xichiou@gmail.com>
2019-06-08 12:05:36 +0800

    check on secure.php

--- show_xoops_var.sh
+++ show_xoops_var.sh
@@ -10,6 +10,25 @@ export PATH
 
 SEARCH_PATH="/var/www/html"
 
+function get_define()
+{
+  parse_file=$1
+  define_name=$2
+
+  get1=$(cat $1|grep "define(" |sed 's/^[ \t]*//g'|sed -e '/^\/\//d'|sed -e '/^#/d'|sed s/\"/\'/g |grep $define_name)
+  get2=$(echo $get1|cut -d"," -f 2|cut -d"'" -f 2)
+  echo $get2
+}
+
+function get_var()
+{
+  parse_file=$1
+  var_name=$2
+
+  get1=$(cat $1|sed s/\"/\'/g|grep \'$var_name|cut -d"=" -f 2|cut -d"'" -f 2)
+  echo $get1
+}
+
 # echo -e "\n\a這個程式腳本幫助您檢查網站運行的版本並且更新："
 # echo -e "XOOPS核心==>版本 ${XOOPS_CORE}"
 # echo -e "[模組]站長工具箱==>版本 ${TAD_ADM_VERSION}"
@@ -37,7 +56,7 @@ echo -e "\n目前網站版本如下:\n====================="
 
 cd $SEARCH_PATH
 
-XOOPS_ROOT_PATH=$(cat mainfile.php |grep "define('XOOPS_ROOT_PATH'"|cut -d"," -f 2|cut -d"'" -f 2)
+XOOPS_ROOT_PATH=$(get_define ${SEARCH_PATH}/mainfile.php XOOPS_ROOT_PATH)
 echo -e "[設定]XOOPS_ROOT_PATH: \e[32m$XOOPS_ROOT_PATH\e[0m"
 if [ $XOOPS_ROOT_PATH != $SEARCH_PATH ]; then
   echo "XOOPS_ROOT_PATH 和實際的網站資料夾不同，可能無法運作!"
@@ -45,33 +64,45 @@ if [ $XOOPS_ROOT_PATH != $SEARCH_PATH ]; then
 fi
 
 
-XOOPS_VAR_PATH=$(cat mainfile.php |grep "define('XOOPS_VAR_PATH'"|cut -d"," -f 2|cut -d"'" -f 2)
+XOOPS_VAR_PATH=$(get_define ${SEARCH_PATH}/mainfile.php XOOPS_VAR_PATH)
 echo -e "[設定]XOOPS_VAR_PATH: \e[32m$XOOPS_VAR_PATH\e[0m"
-XOOPS_PATH=$(cat mainfile.php |grep "define('XOOPS_PATH'"|cut -d"," -f 2|cut -d"'" -f 2)
+
+XOOPS_PATH=$(get_define ${SEARCH_PATH}/mainfile.php XOOPS_PATH)
 echo -e "[設定]XOOPS_PATH: \e[32m$XOOPS_PATH\e[0m"
 
-XOOPS_URL=$(cat mainfile.php |grep "define('XOOPS_URL"|sed 's/^[ \t]*//g'|sed -e '/^\/\//d'|grep "define('XOOPS_URL'"|cut -d"," -f 2|cut -d"'" -f 2)
+XOOPS_URL=$(get_define ${SEARCH_PATH}/mainfile.php XOOPS_URL)
 echo -e "[設定]XOOPS_URL: \e[32m$XOOPS_URL\e[0m"
 
-TAD_ADM_VERSION_CURRENT=$(cat modules/tad_adm/xoops_version.php |grep "'version"|cut -d"=" -f 2|cut -d";" -f 1|cut -d"'" -f 2)
+TAD_ADM_VERSION_CURRENT=$(get_var modules/tad_adm/xoops_version.php version)
 echo -e "[模組]站長工具箱 版本: \e[32m${TAD_ADM_VERSION_CURRENT}\e[0m"
 
-
-TADTOOLS_VERSION_CURRNET=$(cat modules/tadtools/xoops_version.php |grep "'version"|cut -d"=" -f 2|cut -d";" -f 1|cut -d"'" -f 2)
+TADTOOLS_VERSION_CURRNET=$(get_var modules/tadtools/xoops_version.php version)
 echo -e "[模組]tadtools 版本: \e[32m${TADTOOLS_VERSION_CURRNET}\e[0m"
 
-XOOPS_VERSION_CURRENT=$(cat include/version.php |grep "'XOOPS_VERSION"|cut -d"," -f 2|cut -d"'" -f 2)
+XOOPS_VERSION_CURRENT=$(get_define ${SEARCH_PATH}/include/version.php XOOPS_VERSION)
 echo -e "[核心]XOOPS: \e[32m${XOOPS_VERSION_CURRENT}\e[0m"
 
-XOOPS_DB_USER=$(cat ${XOOPS_VAR_PATH}/data/secure.php |grep "'XOOPS_DB_USER"|cut -d"," -f 2|cut -d"'" -f 2)
+if [ -f ${XOOPS_VAR_PATH}/data/secure.php ]; then
+  secure_file=${XOOPS_VAR_PATH}/data/secure.php
+else
+  secure_file=${SEARCH_PATH}/mainfile.php
+fi
+
+XOOPS_DB_USER=$(get_define $secure_file XOOPS_DB_USER)
 echo -e "[設定]XOOPS_DB_USER 資料庫使用者: \e[32m${XOOPS_DB_USER}\e[0m"
 
-XOOPS_DB_PASS=$(cat ${XOOPS_VAR_PATH}/data/secure.php |grep "'XOOPS_DB_PASS"|cut -d"," -f 2|cut -d"'" -f 2)
+XOOPS_DB_PASS=$(get_define $secure_file XOOPS_DB_PASS)
 echo -e "[設定]XOOPS_DB_PASS 資料庫密碼: \e[32m${XOOPS_DB_PASS}\e[0m"
 
-XOOPS_DB_NAME=$(cat ${XOOPS_VAR_PATH}/data/secure.php |grep "'XOOPS_DB_NAME"|cut -d"," -f 2|cut -d"'" -f 2)
+XOOPS_DB_NAME=$(get_define $secure_file XOOPS_DB_NAME)
 echo -e "[設定]XOOPS_DB_NAME 資料庫名稱: \e[32m${XOOPS_DB_NAME}\e[0m"
 
-XOOPS_DB_PREFIX=$(cat ${XOOPS_VAR_PATH}/data/secure.php |grep "'XOOPS_DB_PREFIX"|cut -d"," -f 2|cut -d"'" -f 2)
+XOOPS_DB_PREFIX=$(get_define $secure_file XOOPS_DB_PREFIX)
 echo -e "[設定]XOOPS_DB_PREFIX 資料庫前置碼: \e[32m${XOOPS_DB_PREFIX}\e[0m"
+
+XOOPS_DB_CHARSET=$(get_define $secure_file XOOPS_DB_CHARSET)
+echo -e "[設定]XOOPS_DB_CHARSET 資料庫編碼: \e[32m${XOOPS_DB_CHARSET}\e[0m"
+
+XOOPS_DB_TYPE=$(get_define $secure_file XOOPS_DB_TYPE)
+echo -e "[設定]XOOPS_DB_TYPE 資料庫: \e[32m${XOOPS_DB_TYPE}\e[0m"
 
